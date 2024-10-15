89928a9a217895adc6f9c5447b9a9174e803e61f
= <ricksarkar2005@gmail.com>
2024-10-15 15:48:36 +0530

    Error fixed

--- client1/src/components/Profilebar.jsx
+++ client1/src/components/Profilebar.jsx
@@ -12,7 +12,7 @@ export default function Profilebar() {
     const dbtoken = localStorage.getItem("dbtoken");
     console.log("Fetching profile", dbtoken);
     try {
-      const response = await axios.post("https://ricksarkar.getskybuy.shop/api/profile", {
+      const response = await axios.post("http://localhost:3000/api/profile", {
         Token: dbtoken
       });
       console.log("Profile response", response.data);
